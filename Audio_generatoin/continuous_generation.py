384fdb4a369abb5b038a23b18efbda8e8be314d6
RanjitDas-IN <ranjitdax89@gmail.com>
2025-09-20 20:20:43 +0530

    adding data

--- Audio_generatoin/continuous_generation.py
+++ Audio_generatoin/continuous_generation.py
@@ -1,13 +1,9 @@
-# en-US-AdamMultilingualNeural
-# en-US-AlloyTurboMultilingualNeural
-# en-US-AvaMultilingualNeural
-
-
 import pandas as pd
 import edge_tts
 import asyncio
 import os
 import re
+import time
 
 # Load dataset (choose long or single)
 df = pd.read_csv("data/long_wake_up_dataset.csv", sep="|")  # or "single_wake_up_dataset.csv"
@@ -15,7 +11,7 @@ df = pd.read_csv("data/long_wake_up_dataset.csv", sep="|")  # or "single_wake_up
 # Load voices list
 voices_df = pd.read_csv("Audio_generatoin/egde_tts.csv")
 voices_list = voices_df["voices"].str.strip().tolist()
-print(voices_list)
+
 def sanitize_filename(text):
     # Remove non-alphanumeric characters for filenames
     text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
@@ -23,13 +19,18 @@ def sanitize_filename(text):
     return text
 
 async def generate_voices_for_voice(voice_name):
-    output_dir = os.path.join("long_voices", voice_name)
+    output_dir = os.path.join("short_voices", voice_name)
     os.makedirs(output_dir, exist_ok=True)
     
     for idx, row in df.iterrows():
         text = row["text"]
         file_name = f"{str(idx+1).zfill(3)}_{sanitize_filename(text)}.mp3"
         output_path = os.path.join(output_dir, file_name)
+        
+        # ✅ Skip if file already exists
+        if os.path.exists(output_path):
+            # print(f"[{voice_name}] Skipped (already exists): {output_path}")
+            continue
         
         communicate = edge_tts.Communicate(text, voice_name)
         await communicate.save(output_path)
@@ -43,3 +44,4 @@ async def generate_all_voices():
 
 # Run the async function
 asyncio.run(generate_all_voices())
+time.sleep(50)
--- data_cleaning/clean_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r"/home/ranjit/Desktop/projects/Hello_Nisha/data/long_wake_up_dataset.csv", sep = "|")
-# print(df.head())
-
-dff = df["text"]
-
-print(dff)
